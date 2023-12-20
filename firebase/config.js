12f3b7da90be3b5c2629efefbee76b473a457503
Muhammad Abdullah Hafizh <75511217+MAHafizh@users.noreply.github.com>
2023-12-21 04:27:59 +0700

    Delete firebase directory

--- firebase/config.js
+++
@@ -1,8 +0,0 @@
-const admin = require("firebase-admin");
-const credential = require("./creds.json");
-
-admin.initializeApp({
-    credential: admin.credential.cert(credential),
-});
-
-module.exports = admin;
